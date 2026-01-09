f3489de77ad0633518be5c38f0292ee18d4a7b54
Matthew Russell <matthewjosephrussell@gmail.com>
2026-01-08 16:26:23 -0800

    v0.4.0

--- src/cli.ts
+++ src/cli.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name('steam')
   .description('CLI tool for browsing and discovering Steam games')
-  .version('0.3.0');
+  .version('0.4.0');
 
 // Register commands
 program.addCommand(createConfigCommand());
